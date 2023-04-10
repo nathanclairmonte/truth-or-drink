9a0f406701192a0388812842d05a97a50e4c5892
nathanclairmonte <nathanclairmonte@outlook.com>
2023-04-10 16:49:14 -0400

    added preliminary questions

---
+++ src/data/questions1.js
@@ -0,0 +1,79 @@
+// export const questions = [
+//     `Give us a passionate example of your dirty talk.`,
+//     `Would you ever knowingly enter into an open relationship?
+//      What rules are required to keep the peace?`,
+//     `Where’s the strangest place
+//      you’ve had sex?`,
+//     `What’s the sex skill you’re
+//      most proud of?`,
+//     `Describe your sex life with
+//      a movie title, and tell us
+//      your reasoning.`,
+//     `Have you ever done butt
+//      stuff? If not, would you
+//      like to?`,
+//     `Have you ever had a
+//      threesome? Would you?`,
+//     `Tell us about an awkward
+//      or inappropriate sex dream
+//      you’ve had. Was anyone
+//      here involved?`,
+//     `When was the last time you
+//      were tested for a sexually
+//      transmitted infection?
+//      What were the results?`,
+//     `Have I ever popped into
+//      your head while you were
+//      masturbating? Will it
+//      happen now that we’ve
+//      asked the question?`,
+//     `Who do you think has had
+//      more sexual partners, you
+//      or me?`,
+//     `What’s the messiest thing
+//      you’ve ever done sexually?`,
+//      `What was your best
+//      orgasm? Was it with
+//      yourself, or someone else?`,
+//      `If we were stuck on a
+//      desert island, would
+//      we become lovers?`,
+//      `Describe your first time
+//      having sex. Include every
+//      cringey detail.`,
+//      ``,
+//      ``,
+//      ``,
+//      ``,
+//      ``,
+//      ``,
+//      ``,
+//      ``,
+//      ``,
+// ];
+
+export const questions = [
+    "Where’s the strangest place you’ve had sex?",
+    "What’s the sex skill you’re most proud of?",
+    "Have you ever done butt stuff? If not, would you like to?",
+    "When was the last time you were tested for a sexually transmitted infection? What were the results?",
+    "Have I ever popped into your head while you were masturbating? Will it happen now that we’ve asked the question?",
+    "Have you ever been caught masturbating or having sex?",
+    "Have you ever cheated on anyone? What were the circumstances?",
+    "Have you ever had a one-night stand?",
+    "Who do you think will have more sexual partners by the time we die, you or me?",
+    "Who here would have the best sexual chemistry with me? Based upon what evidence?",
+    "What’s something you wish your ex would have done sexually, but didn’t?",
+    "Have I gotten sexier since you’ve known me?",
+    "If I cheat on my partner, is it absolutely essential that I tell them? Why or why not?",
+    "Have you ever walked in on your parents doing that thing that grownups do when they love each other very much?",
+    "What was your most shame-filled orgasm?",
+    "True or false: sometimes it’s okay to have unprotected sex. Explain your reasoning.",
+    "Have you ever had sex with someone who was already in a committed relationship?",
+    "If I had a hot brother/sister, would you let them seduce you?",
+    "Have you ever slid into someone’s DMs? Any techniques you want to share?",
+    "Assuming we were both single, would you date me?",
+    "Have you ever feared for your life or safety while drinking or on drugs? Take us to the scene.",
+    "How often are you intoxicated when you have sex? How often are you sober? What’s the difference?",
+    "Do you find me physically attractive?",
+];
